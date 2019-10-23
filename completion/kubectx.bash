11c47001369da9d2b67d26a681184f3f82fdb9f7
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2019-10-23 15:00:20 -0300

    kubectx

--- completion/kubectx.bash
+++ completion/kubectx.bash
@@ -2,7 +2,13 @@ _kube_contexts()
 {
   local curr_arg;
   curr_arg=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "- $(kubectl config get-contexts --output='name')" -- $curr_arg ) );
+
+  if [ -z $2 ]; then
+      COMPREPLY=( $(compgen -W "- $(kubectl config get-contexts --output='name')" -- $curr_arg ) );
+  else
+      COMPREPLY=$(kubectl config get-contexts --output='name' | fzf --ansi -q $2 -1);
+  fi
+  
 }
 
 complete -F _kube_contexts kubectx kctx ctx
