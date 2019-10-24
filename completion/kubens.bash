af0b13349a934812eb02285a269e5cedfda8ffec
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2019-10-24 13:04:59 -0300

    bash completion ns

--- completion/kubens.bash
+++ completion/kubens.bash
@@ -1,8 +1,13 @@
-_kube_namespaces()
+ _kube_namespaces()
 {
   local curr_arg;
   curr_arg=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')" -- $curr_arg ) );
-}
 
+  if [ -z $2 ]; then
+      COMPREPLY=( $(compgen -W "- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}')" -- $curr_arg ) );
+  else
+      COMPREPLY=( $(compgen -W "- $(kubectl get namespaces -o=jsonpath='{range .items[*].metadata.name}{@}{"\n"}{end}' | fzf --ansi -q $2 -1 )" -- $curr_arg ) );
+  fi
+
+}
 complete -F _kube_namespaces kubens kns ns
